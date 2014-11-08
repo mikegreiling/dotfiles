4af5f4a2d40c0aa350bae1d5d63e538302e5b8c9
Mike Greiling <mike@pixelcog.com>
2014-11-08 00:13:38 -0600

    modify bootstrap to run homebrew/cask installations and perform os tweaks. rename osx script for consistancy

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,17 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "brew.sh" --exclude "cask.sh" --exclude "init/" --exclude "osx.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
+
+	# If running OSX install homebrew, cask, and system tweaks
+	if [[ "$(uname)" == "Darwin" ]]; then
+		chflags hidden ~/bin
+		source brew.sh
+		source cask.sh
+		source osx.sh
+	fi;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
