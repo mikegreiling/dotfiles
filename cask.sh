6e888662da072d910e776fa2e4f4e6d0340a6b1c
Mike Greiling <mike@pixelcog.com>
2014-11-08 00:12:23 -0600

    implement cask install script to automate osx app installation

---
+++ cask.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Exit if Homebrew is not installed.
+[[ ! "$(type -P brew)" ]] && echo "Homebrew is not installed." && return 1
+
+# Ensure the cask keg and recipe are installed.
+brew tap caskroom/cask
+brew install brew-cask
+
+# Exit if, for some reason, cask is not installed.
+[[ ! "$(brew ls --versions brew-cask)" ]] && echo "Brew-cask failed to install." && return 1
+
+# Apps
+apps=(
+	asepsis
+	atom
+	caffeine
+	firefox
+	flash
+	google-chrome
+	kaleidoscope
+	things
+	transmission
+	transmit
+	vagrant
+	virtualbox
+	vlc
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# Setup Atom.io plugins (apm should be installed with atom)
+[[ ! "$(type -P apm)" ]] && echo "Atom Package Manager failed to install." && return 1
+
+# Atom packages
+atom_pkgs=(
+	codebug
+	editorconfig
+	minimap
+)
+
+apm install ${atom_pkgs[@]}
