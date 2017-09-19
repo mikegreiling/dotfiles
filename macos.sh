b5a6df7ffaf46dee911acdb85c7b411d411aafaa
Mike Greiling <mike@pixelcog.com>
2017-09-19 01:20:01 -0500

    remove sleepimage hacks

--- macos.sh
+++ macos.sh
@@ -130,20 +130,6 @@ sudo rm -rf /System/Library/CoreServices/DefaultDesktop.jpg
 sudo ln -s /Library/Desktop\ Pictures/Milky\ Way.jpg /System/Library/CoreServices/DefaultDesktop.jpg
 
 ###############################################################################
-# SSD-specific tweaks                                                         #
-###############################################################################
-
-# Disable hibernation (speeds up entering sleep mode)
-# sudo pmset -a hibernatemode 0
-
-# Remove the sleep image file to save disk space
-# sudo rm /private/var/vm/sleepimage
-# Create a zero-byte file instead…
-# sudo touch /private/var/vm/sleepimage
-# …and make sure it can’t be rewritten
-# sudo chflags uchg /private/var/vm/sleepimage
-
-###############################################################################
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
 ###############################################################################
 
