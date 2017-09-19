a00bb5011fea18a3b478f5b4bf738e8a2f66799f
Mike Greiling <mike@pixelcog.com>
2017-09-19 17:09:37 -0500

    cleanup homebrew app installation

--- cask.sh
+++ cask.sh
@@ -4,52 +4,31 @@
 [[ ! "$(type -P brew)" ]] && echo "Homebrew is not installed." && return 1
 
 # Ensure the cask keg and recipe are installed.
-brew tap caskroom/cask
-brew install brew-cask
+if [[ ! "$(brew ls --versions brew-cask)" ]]; then
+	brew tap caskroom/cask
+	brew install brew-cask
+fi
 
-# Exit if, for some reason, cask is not installed.
-[[ ! "$(brew ls --versions brew-cask)" ]] && echo "Brew-cask failed to install." && return 1
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-# Apps
-apps=(
-	arq
-	asepsis
-	atom
-	beardedspice
-	bettertouchtool
-	bitbar
-	fantastical
-	firefox
-	flash
-	flux
-	google-chrome
-	kaleidoscope
-	keepingyouawake
-	moom
-	packer
-	things
-	transmission
-	transmit
-	vagrant
-	virtualbox
-	vlc
-)
+brew cask install arq
+brew cask install asepsis
+brew cask install beardedspice
+brew cask install bettertouchtool
+brew cask install bitbar
+brew cask install fantastical
+brew cask install firefox
+brew cask install google-chrome
+brew cask install kaleidoscope
+brew cask install keepingyouawake
+brew cask install packer
+brew cask install things
+brew cask install transmission
+brew cask install transmit
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install visual-studio-code
+brew cask install vlc
 
-# Install apps to /Applications
-# Default is: /Users/$user/Applications
-brew cask install --appdir="/Applications" ${apps[@]}
-
-# Setup Atom.io plugins (apm should be installed with atom)
-[[ ! "$(type -P apm)" ]] && echo "Atom Package Manager failed to install." && return 1
-
-# Atom packages
-atom_pkgs=(
-	codebug
-	dash
-	editorconfig
-	minimap
-	term2
-	graphite-ui
-)
-
-apm install ${atom_pkgs[@]}
+# Remove outdated versions from the cellar.
+brew cask cleanup
