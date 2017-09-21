511b113224d755404a0ac62caa36599fb5d36df3
Mike Greiling <mike@pixelcog.com>
2017-09-20 23:32:05 -0500

    remove superfluous --all option from brewk upgrade command

--- brew.sh
+++ brew.sh
@@ -6,7 +6,7 @@ if [[ ! "$(type -P brew)" ]]; then
 fi
 
 brew update
-brew upgrade --all
+brew upgrade
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
