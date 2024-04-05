d0dcff056e17bd0dbf6f9d3c7426f298dabbaf99
Mike Greiling <mike@pixelcog.com>
2024-04-05 18:23:16 -0500

    Add empty chezmoi pre-source-state hook

---
+++ home/.chezmoihooks/pre-source-state.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# This is where we install things that are required for our chezmoi scripts to
+# run e.g. our password manager or other dependencies that must exist before
+# chezmoi compiles templates.
+
+# This script is run before every single command so it must be fast
