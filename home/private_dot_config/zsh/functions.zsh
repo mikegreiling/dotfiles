fb5e5fcb902928b7fe2233e112597cfc4848db3e
Mike Greiling <mike@pixelcog.com>
2024-04-18 16:12:35 -0500

    Credit the repo where I learned this trick

--- home/private_dot_config/zsh/functions.zsh
+++ home/private_dot_config/zsh/functions.zsh
@@ -1,5 +1,6 @@
 
 # re-run all chpwd and precmd hooks and tell p10k to refresh the prompt
+# https://github.com/oxcl/.dotfiles/blob/79317b7cfa/home/.config/zsh/custom/p10k_custom.plugin.zsh#L4-L10
 refresh-prompt() {
   local f
   for f in chpwd "${chpwd_functions[@]}" precmd "${precmd_functions[@]}"; do
